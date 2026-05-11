1c520bc53f8299bd5773f0ced2f828c116dd1c2e
Leticia Paola <leticiapaolasouza@gmail.com>
2026-05-11 20:18:22 -0300

    Aula 11/05

---
+++ scripts/05-probabilidade.R
@@ -0,0 +1,17 @@
+# Arquivo: 05-probabilidade.R
+# Autor: Joao Silva
+# Data: 11/05/2026
+# Objetivos:
+# 1. Praticar cálculos básicos de probabilidades
+# 2. Praticar conceitos básicos de simulação de Monte Carlo
+
+# Configuracoes globais -----------------------------------------------
+
+# exibe números sem notação científica
+options(digits = 5, scipen = 999)
+
+
+# Pacotes usados ------------------------------------------------------
+
+library(tidyverse)
+library(probs) # instale esse pacote
